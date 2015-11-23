c5919226464178ecda0ba5ba6aea01cc75cf4e93
Kazuki Yoshida <kzkysdjpn@gmail.com>
2015-11-24 01:05:55 +0900

    Add setup for Wowza Media Server.

--- livereporter/scripts/wowza.js
+++ livereporter/scripts/wowza.js
@@ -14,7 +14,7 @@ function setupLanguage(lang)
 	setupLanguageDialog();
 	$.ajax({
 		type:         'get',
-		url:          'lang/' + lang + '/index.json',
+		url:          'lang/' + lang + '/wowza.json',
 		contentType:  'application/JSON',
 		dataType:     'JSON',
 		scriptCharset:'utf-8',
@@ -34,21 +34,24 @@ function loadLanguage(data)
 	var	i;
 	var	LANG_KEY = [
 		"TITLE",
-		"TITLE_MSG",
-		"OTHER_FUNCTION",
-		"BROADCAST_SERVER",
-		"USE_CLIENT_APP",
-		"USE_CLIENT_APP_MSG0",
-		"USE_CLIENT_APP_MSG1",
-		"USE_CLIENT_APP_MSG2",
-		"USE_CLIENT_APP_MSG3",
-		"USE_SERVER_PUSH",
-		"USE_SERVER_PUSH_MSG0",
-		"USE_SERVER_PUSH_MSG1",
-		"USE_SERVER_PUSH_MSG2"
+		"MSG_0",
+		"MSG_1",
+		"MSG_2",
+		"MSG_3",
+		"MSG_4",
+		"MSG_5",
+		"MSG_6",
+		"MSG_7",
+		"MSG_8"
+	];
+	var	LANG_KEY_CLASS = [
+		"MAIN_RETURN"
 	];
 	for(i = 0; i < LANG_KEY.length; i++){
 		$('#' + LANG_KEY[i]).html(data[LANG_KEY[i]]);
+	}
+	for(i = 0; i < LANG_KEY_CLASS.length; i++){
+		$('.' + LANG_KEY_CLASS[i]).html(data[LANG_KEY_CLASS[i]]);
 	}
 	return;
 }

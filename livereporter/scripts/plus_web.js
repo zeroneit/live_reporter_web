01f43452e5a9a8a5f10e6de0b3a11e27bf32aba2
Kazuki Yoshida <kzkysdjpn@gmail.com>
2015-11-29 01:51:59 +0900

    WEB interface page checkin.

---
+++ livereporter/scripts/plus_web.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	setBaseLanguage(setupIndex);
+	return;
+});
+
+function setupIndex(lang)
+{
+	setupLanguage(lang);
+	return;
+}
+
+function setupLanguage(lang)
+{
+	setupLanguageDialog();
+	$.ajax({
+		type:         'get',
+		url:          'lang/' + lang + '/plus_web.json',
+		contentType:  'application/JSON',
+		dataType:     'JSON',
+		scriptCharset:'utf-8',
+		success:      function(data){
+			loadLanguage(data);
+			return;
+		},
+		error:        function(data){
+			return;
+		}
+	});
+	return;
+}
+
+function loadLanguage(data)
+{
+	var	i;
+	var	LANG_KEY = [
+		"TITLE",
+		"MSG_0",
+		"MSG_1",
+		"MSG_2",
+		"MSG_3",
+		"MSG_4",
+		"MSG_5"
+	];
+	var	LANG_KEY_CLASS = [
+		"MAINVIEW_RETURN"
+	];
+	for(i = 0; i < LANG_KEY.length; i++){
+		$('#' + LANG_KEY[i]).html(data[LANG_KEY[i]]);
+	}
+	for(i = 0; i < LANG_KEY_CLASS.length; i++){
+		$('.' + LANG_KEY_CLASS[i]).html(data[LANG_KEY_CLASS[i]]);
+	}
+	return;
+}
